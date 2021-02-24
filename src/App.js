3f04a6fbed1e0190b24ef14cfbcce43ecbc0bd58
yKey <ykey5224@gmail.com>
2021-02-24 20:38:49 +0900

    2021-02-24 20:38

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      LOL Report
-    <input type="text" name="text1"/>
-    <input type="button" value="BTN1"/>
-    </div>
-  );
+import React from "react";
+import axios from "axios";
+class App extends React.Component {
+  state = {
+    isLoading:true,
+    summoner:[]
+  };
+  getSummonerInfo = async () =>{
+    const summoner = await axios.get("https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/판다삐?api_key=RGAPI-d81d907d-179a-4725-9dc3-2c53531a1af1");
+    console.log(summoner);
+  };
+  componentDidMount(){
+    setTimeout(() =>{
+      this.setState({isLoading : false})
+    },6000);
+    this.getSummonerInfo();
+  }
+  render() {
+    const { isLoading } = this.state;
+    return (
+      <div>{isLoading ? "Loading..." : "We are ready"}</div>
+    );
+  }
 }
-
 export default App;
