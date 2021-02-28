9a3f7ca38fdc3679e645835501fb06fdb9738b99
yKey <ykey5224@gmail.com>
2021-02-28 17:14:20 +0900

    Header도 cont-prst 패턴으로 바꾸자

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import { Link, withRouter } from "react-router-dom";
 
-// export default withRouter(({ history }) => (
-//   <div>
-//         <input type="text" id ="sumonnerName"/>
-          
-//           <a href="/#/search/판다삐" >판다삐 ㄱ</a>
-//   </div>
-// ));
-
-
 class Header extends React.Component {
     state={
         summonerName:''
@@ -19,12 +10,6 @@ class Header extends React.Component {
             summonerName: e.target.value
         });
     }
-    searchClick = () => {
-        console.log(`summonerName는:${this.state.summonerName}`);
-        //history.pushState(`/#/search/${this.state.summonerName}`)
-        this.props.history.push(`/search/${this.state.summonerName}`);
-        console.log(this.props);
-    }
     render() {
         const { summonerName } = this.state;
         const { inputChange, searchClick } = this;
@@ -32,7 +17,9 @@ class Header extends React.Component {
         <div className="Header">
           <header className="App-header">
             <input type="text" value={summonerName} placeholder="소환사이름" onChange={inputChange} />
-            <button onClick={searchClick}>Search</button>
+            <Link to={`/search/${this.state.summonerName}`}>
+                <button>Search</button>
+            </Link>
           </header>
         </div>
       );
